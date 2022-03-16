793d065aa8196fc638104111351680e2847f6aca
Toyosi Olaribigbe <dxo028@cs.bham.ac.uk>
2022-03-16 16:06:41 +0000

    Update settings.py

--- teammatcherproject/settings.py
+++ teammatcherproject/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': env('DATABASE_USER'), 
         'PASSWORD': env('DATABASE_PASS'),
         'HOST': env('DATABASE_HOST'), 
-        'PORT': '5432',
+        'PORT': '5434',
     }
 }
 
