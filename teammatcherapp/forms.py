bf682b4097c2b1b133a2c8f8d976282cc127314a
Guanzhi Chen <gxc012@cs.bham.ac.uk>
2022-03-17 11:34:28 +0000

    Update forms.py

--- teammatcherapp/forms.py
+++ teammatcherapp/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User as u
 
 class searchdetail(forms.ModelForm):
      modeselect = (('team','team'),('teammember','teammember'),('compete','compete'))
-     mode = forms.ChoiceField(choices = modeselect,widget=forms.RadioSelect(attrs={'class': "custom-radio-list"}))
+     mode = forms.ChoiceField(choices = modeselect,widget=forms.RadioSelect)
      class Meta: 
          model = User
          fields = ['sport','postcode']
