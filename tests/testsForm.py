86600ff4a116bbbda84cd42e9612edbb67c1f17a
Kuzie <kuziechen@outlook.com>
2022-03-30 19:19:51 +0100

    change test

--- tests/testsForm.py
+++ tests/testsForm.py
@@ -11,22 +11,22 @@ class Formtests(TestCase):
          #one = User(name = 'David Olaribigbe',postcode ='E9 B2S',gender = 'Female' ,phone = '07745863521',age = 17 ,sport ='football',experience = 'advanced',username = 'price',password='paword')
          #one = User.objects.get(name ='David Olaribigbe')
          #data = {'name': one.name, 'postcode': one.postcode, 'gender': one.gender, 'phone': one.phone, 'age': one.age,'sport':one.sport,'experience':one.experience,'username':one.username,'password':one.password,'mode':one.mode}
-         response = self.client.post('/register/',data = {'name': 'David Olaribigbe', 'postcode': 'E9 B2S', 'gender': 'Female', 'phone': '07745863521', 'age': 17,'sport': 'football','experience': 'advanced','username': 'price','password': 'paword','mode': 'team'})
-         form = RegisterForm({'name': 'David', 'postcode': 'E9 B2S', 'Gender': 'Female', 'phone': '07745863521', 'age': 17, 'Sport': 'football','Experience': 'advanced','username': 'price','password': 'paword','mode': 'team'})
+         response = self.client.post('/register/',data = {'name': 'David Olaribigbe', 'city': 'Birmingham', 'gender': 'Female', 'phone': '07745863521', 'age': 17,'sport': 'football','experience': 'advanced','username': 'price','password': 'paword','mode': 'team'})
+         form = RegisterForm({'name': 'David', 'city': 'Birmingham', 'Gender': 'Female', 'phone': '07745863521', 'age': 17, 'Sport': 'football','Experience': 'advanced','username': 'price','password': 'paword','mode': 'team'})
          self.assertFalse(form.is_valid())
 
     def test_invalid_input(self):
-        form = RegisterForm({ 'name':'Dave','postcode':'E9 B2S','gender':'Female','phone':'0774586356765563','age':17,'sport':'football','experience':'advanced','username':'price','password':'paword','mode':'team'})
+        form = RegisterForm({ 'name':'Dave','city':'Birmingham','gender':'Female','phone':'0774586356765563','age':17,'sport':'football','experience':'advanced','username':'price','password':'paword','mode':'team'})
         self.assertFalse(form.is_valid())
     
     def test_valid_searchform(self):
-       form = searchdetail({ 'sport':'football','postcode':'e9 6lg','mode':'team'})
+       form = searchdetail({ 'sport':'football','city':'Birmingham','mode':'team'})
        self.assertTrue(form.is_valid())
 
     def test_invalid_searchform(self):
-        form = searchdetail({ 'sport':'football','postcode':' ','mode':'team'})
+        form = searchdetail({ 'sport':'football','city':' ','mode':'team'})
         self.assertFalse(form.is_valid())
 
     def test_outofbounds_searchform(self):
-        form = searchdetail({ 'sport':'football','postcode':'ABCD123456Q','mode':'team'})
+        form = searchdetail({ 'sport':'football','city':'ABCD123456Q','mode':'team'})
         self.assertFalse(form.is_valid())
