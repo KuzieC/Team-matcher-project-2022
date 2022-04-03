e02da86d023dcb29b45f59f0f3bce63e84756adc
Guanzhi Chen <gxc012@cs.bham.ac.uk>
2022-04-03 17:13:21 +0000

    Update teammatcherproject/settings.py

--- teammatcherproject/settings.py
+++ teammatcherproject/settings.py
@@ -47,8 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teammatcherapp',
-    'widget_tweaks',
-    'crispy_forms',
     'rest_framework',
     'rest_framework.authtoken',
     'api',
